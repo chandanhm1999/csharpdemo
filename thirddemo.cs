9cc61387a56fab43c59329cd5440c58c57b4424f
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2024-04-23 17:02:26 +0530

    Update thirddemo.cs

--- thirddemo.cs
+++ thirddemo.cs
@@ -14,5 +14,18 @@ public class HelloWorld
         int userName = 20;
         Console.WriteLine ("username value is: " + username);
         Console.WriteLine ("userName value is: " + userName);
+
+        // Lgical Operators
+        bool result;
+       int n1 = 10, n2 = 20;
+       
+       // OR operator
+       
+       result = (n1 == n2) || (n1 > 5);
+       Console.WriteLine(result + "\n");
+       
+       // AND operator
+       result = (n1 == n2) && (n1 > 5);
+       Console.WriteLine("\n" + result);
     }
 }
