09d34b8d9de627b7202d5fe63a9c12f09dfc8136
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2024-04-23 16:16:29 +0530

    Create fourthdemo.cs

---
+++ fourthdemo.cs
@@ -0,0 +1,20 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //Array with index
+       int[] rollNumber = {1, 2, 3, 4, 5};
+       int total = rollNumber[0]+
+       rollNumber[1]+
+       rollNumber[2]+
+       rollNumber[3]+
+       rollNumber[4];
+       
+       Console.WriteLine("Total rollnumber exe:" + total);
+    }
+}
