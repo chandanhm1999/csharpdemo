09f094d9eccedd8f2d54168e1612c248071f3c0f
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2024-04-23 11:27:42 +0530

    Create firstday.cs

---
+++ firstday.cs
@@ -0,0 +1,26 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Namsthe");
+        Console.Write("Pelase enter your name:");
+        string firstName = Console.ReadLine();
+        Console.Write("Pelase enter your LastName:");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("Namasthe, {0} {1}!", firstName, lastName);
+        
+    }
+}
+
+/* Namsthe
+Pelase enter your name:sxasxs
+Pelase enter your LastName:asxasx
+Namasthe, sxasxs asxasx!
+
+=== Code Execution Successful ===
+*/
