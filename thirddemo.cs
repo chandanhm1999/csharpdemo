742ed47de0d9484766265a958d5259d5e7eb173b
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2024-04-23 12:34:34 +0530

    Create thirddemo.cs

---
+++ thirddemo.cs
@@ -0,0 +1,18 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        
+        //caseSensite
+        
+        int username = 10;
+        int userName = 20;
+        Console.WriteLine ("username value is: " + username);
+        Console.WriteLine ("userName value is: " + userName);
+    }
+}
