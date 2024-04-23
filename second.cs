a1f5d3c0d0878b939c0f89eb51d59b5e615238a7
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2024-04-23 11:48:55 +0530

    Create second.cs

---
+++ second.cs
@@ -0,0 +1,33 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int a; //Declaration
+        int b=10; //Initialization
+        b=20; //Manupulate we can chaneg the value with latest
+        
+         
+        Console.WriteLine (b);
+        
+        //Data types
+        
+        byte b1= 255; //0 to 255
+        int i1 = 21474364; // -21474364
+        float f1= 7.7f;
+        char c1 ='k';
+        bool bl1=true;
+        string s1 = "Vishnu";
+        Console.WriteLine ("Example for int" + i1 + 
+        "\nExample for byte" + b1 + 
+        "\nExample for float" + f1 + 
+        "\nExample for char" + c1 + 
+        "\nExample for bool" + bl1 + 
+        "\nExample for string" + s1 );
+        
+    }
+}
