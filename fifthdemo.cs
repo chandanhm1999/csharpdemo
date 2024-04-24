993f5cc6f112f0de3b79153b449f7f41524c5d27
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2024-04-24 11:53:48 +0530

    Create fifthdemo.cs

---
+++ fifthdemo.cs
@@ -0,0 +1,20 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+
+      //loops if-else & shortcuts
+       int number = 15;
+       
+       if (number% 2 == 0)
+       Console.WriteLine(number +"is even number");
+       
+       else
+       Console.WriteLine( number + "is odd number");
+    }
+}
